df716a3e62cc0e81f82308df7227555fe9e17fa2
jeremythi <jeremy.thiboulet@gmail.com>
2017-12-08 10:07:18 +0100

    Maj cal.js

--- src/calc.js
+++ src/calc.js
@@ -8,9 +8,24 @@
 ** La fonction renvoie null en cas d'erreur
 */
 function calculatrice(signe, gauche, droite) {
-    var result = null;
-
-    return result;
+    if (typeof gauche === 'number' && typeof droite === 'number') {
+           if (gauche>=0 && droite<430 && droite!=0) {
+            switch (signe) {
+                case "*":
+                    return gauche*droite;
+                case "+":
+                    return gauche+droite;
+                case "-":
+                    return gauche-droite;
+                case "/":
+                    return gauche/droite;
+                default:
+                    return null;
+            } 
+        } else {
+               return null;
+        }
+    } else{
+        return null;
+    }
 }
- 
-module.exports = calculatrice;
